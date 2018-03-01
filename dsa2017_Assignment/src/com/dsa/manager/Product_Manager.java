6417fec6049f24975309ae4fea1534c149054419
Duc\Ducc <ducpagc00970@fpt.edu.vn>
2018-03-01 09:13:47 +0700

    Revert "remove commented methods"
    This reverts commit 2b309f67f35274688fa02fcc62ffb15714e045be.

--- dsa2017_Assignment/src/com/dsa/manager/Product_Manager.java
+++ dsa2017_Assignment/src/com/dsa/manager/Product_Manager.java
@@ -8,7 +8,15 @@ import com.dsa.files.List_Products;
 import com.dsa.physical.Tab_Product;
 
 public class Product_Manager {
-
+	// public boolean isCodeExist(String pcode) {
+	// boolean found = false;
+	// for (Tab_Product tp : List_Products.list_products) {
+	// if (tp.getPcode().equals(pcode)) {
+	// found = true;
+	// }
+	// }
+	// return found;
+	// }
 	public boolean isCodeExist(String pcode) {
 		boolean found = false;
 		for (int i = 0; i < List_Products.list_products.size(); i++) {
@@ -41,7 +49,28 @@ public class Product_Manager {
 		return lp;
 	}
 
+	// public Product_List<Tab_Product> search_product(String pcode) {
+	// Product_List<Tab_Product> lp = new Product_List<>();
+	// for (Tab_Product p : List_Products.list_products) {
+	// if (p.getPcode().contains(pcode)) {
+	// // System.out.println(p);
+	// lp.add(p);
+	// }
+	// }
+	// return lp;
+	// }
+
 	public void remove_product(Linked_List<Tab_Product> remove_list) {
+		// for (int i = 0; i < List_Products.list_products.size(); i++) {
+		// Tab_Product p = List_Products.list_products.get(i);
+		// for (int k = 0; k < remove_list.size(); k++) {
+		// Tab_Product p2 = remove_list.get(k);
+		// //System.out.println(p2.getPcode());
+		// if (p.getPcode().equals(p2.getPcode())) {
+		// List_Products.list_products.remove(p);;
+		// }
+		// }
+		// }
 		for (Tab_Product p : remove_list) {
 			List_Products.list_products.remove(p);
 		}
